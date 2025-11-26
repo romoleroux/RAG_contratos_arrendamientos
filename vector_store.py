61b5a7cc21dd9e204e398c1b1eaa7eaba851a6de
Manuel Romoleroux <152623787+ManuelTaBridge@users.noreply.github.com>
2025-11-25 18:08:08 -0600

    script ingestion.py creado

---
+++ vector_store.py
@@ -0,0 +1,31 @@
+# import libraries
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+loader =  PyPDFDirectoryLoader("C:\\Users\\romol\\Documents\\Software\\AI_AGENT\\santiago_ciber\\langhcain_langraph_course\\sistema_RAG\\contratos")
+documentos = loader.load()
+
+print(f'Se cargaron {len(documentos)} documentos desde el directorio.')
+
+# Text splitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=5000,
+    chunk_overlap=1000,
+)
+
+docs_split = text_splitter.split_documents(documentos)
+print(f'Se crearon {len(docs_split)} chunks de texto.')
+
+
+
+vectorstore = Chroma.from_documents(
+    embedding_function=OpenAIEmbeddings(model = "text-embedding-3-small"),
+    documents=docs_split,
+    persist_directory="C:\\Users\\romol\\Documents\\Software\\AI_AGENT\\santiago_ciber\\langhcain_langraph_course\\sistema_RAG\\chroma_db")
+
+print("¡Base de datos creada y guardada con éxito!")
